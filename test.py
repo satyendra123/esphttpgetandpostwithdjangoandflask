50b31307b6e934606322835284af71c68f90f544
satyendra123 <Singhsatyendra885@gmail.com>
2024-05-08 18:40:10 +0530

    Update test.py

--- test.py
+++ test.py
@@ -1,19 +1,3 @@
-'''
-from flask import Flask, request
-
-app = Flask(__name__)
-
-@app.route('/update-sensor', methods=['POST'])
-def update_sensor():
-    data = request.form
-    print("Received data:")
-    for key, value in data.items():
-        print(f"{key}: {value}")
-    return "Data received", 200
-
-if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=1880)
-'''
 from flask import Flask, request
 
 app = Flask(__name__)
